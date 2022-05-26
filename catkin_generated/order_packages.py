38dce88fd715e5b408d0f8693367d073f9742218
studentdonya <80056149+studentdonya@users.noreply.github.com>
2022-05-26 11:39:25 +0200

    pre claenup

--- catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/ros1/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/ros1/devel;/root/jupyter/devel;/root/action/devel;/root/main/devel;/opt/ros/noetic'.split(';') if '/root/ros1/devel;/root/jupyter/devel;/root/action/devel;/root/main/devel;/opt/ros/noetic' != '' else []
