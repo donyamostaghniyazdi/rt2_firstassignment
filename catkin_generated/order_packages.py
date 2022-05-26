44e98cae82b3d65958f40fb8e286da8c624f1c1c
studentdonya <80056149+studentdonya@users.noreply.github.com>
2022-05-26 11:47:39 +0200

    final cleanup

--- catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/root/jupyter/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/root/action/devel;/root/main/devel;/opt/ros/noetic'.split(';') if '/root/action/devel;/root/main/devel;/opt/ros/noetic' != '' else []
