e845ebe72a77f2dd9b699e6973e9a4273e182958
studentdonya <80056149+studentdonya@users.noreply.github.com>
2022-05-26 11:41:23 +0200

    final clean up

---
+++ ros.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/ros/noetic/setup.bash
+source /root/ros1/devel/setup.bash
---
+++ ros12.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /root/ros1/devel/setup.bash
+source /root/ros2/install/setup.bash
